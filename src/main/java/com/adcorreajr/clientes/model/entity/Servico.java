47f6a0bbd4b5333b6a182c3217a894931c3e9d93
adcorrea <antoniopucc@hotmail.com>
2022-09-04 11:12:37 -0300

    Projeto criado

---
+++ src/main/java/com/adcorreajr/clientes/model/entity/Servico.java
@@ -0,0 +1,31 @@
+package com.adcorreajr.clientes.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 255)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @Column
+    private BigDecimal valor;
+}
---
+++ src/main/java/com/adcorreajr/clientes/rest/ClienteController.java
@@ -0,0 +1,60 @@
+package com.adcorreajr.clientes.rest;
+
+import com.adcorreajr.clientes.model.entity.Cliente;
+import com.adcorreajr.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody @Valid Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+        return clienteRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
+    }
+
+    @GetMapping
+    public List<Cliente> acharTodos(){
+        return clienteRepository.findAll();
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        clienteRepository.findById(id)
+                .map(cliente -> {
+                    clienteRepository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody @Valid Cliente cliente){
+        clienteRepository.findById(id)
+                .map(newCliente -> {
+                    cliente.setId(newCliente.getId());
+                    clienteRepository.save(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
+    }
+}
